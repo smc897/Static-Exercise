649c25982fca25b3c0379083cf1041387dc132f2
ScottMC89 <scottmc592@gmail.com>
2023-10-10 14:01:50 -0500

    temp done

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine($"32F={TempConverter.FahrenheitToCelcius(32.0)}C");
+            Console.WriteLine($"32C={TempConverter.CelsiusToFahrenheit(32.0)}F");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double temp) {
+            return (9.0 / 5.0)*(temp - 32);
+        }
+
+        public static double CelsiusToFahrenheit(double temp) {
+            return (9.0 / 5.0) * temp + 32.0;
+        }
+    }
+}
